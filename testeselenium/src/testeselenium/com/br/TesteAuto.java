7a98e8eaf6b2bb325afef1c4342d0f15513a5ac8
Genisson <genissonfg@gmail.com>
2018-06-13 10:37:20 -0300

    Primeiro teste, abrindo página web

--- testeselenium/src/testeselenium/com/br/TesteAuto.java
+++ testeselenium/src/testeselenium/com/br/TesteAuto.java
@@ -1,5 +1,30 @@
 package testeselenium.com.br;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TesteAuto {
+	
+	private WebDriver driver = null;
+
+	
+	@Before
+	public void inicializar() {
+		System.setProperty("webdriver.chrome.driver" , "C:\\Selenium\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+				
+		
+	}
+	
+	
+    @Test
+	public void fluxo() {
+		driver.get("https://adx.doctum.edu.br/");
+		
+    }
+    
+
 
 }
