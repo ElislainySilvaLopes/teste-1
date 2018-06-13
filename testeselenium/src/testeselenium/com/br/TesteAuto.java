0b1c79ab17f245b1e23ce27c1d7a535dd71a6e4f
Genisson <genissonfg@gmail.com>
2018-06-13 12:01:25 -0300

    Segundo teste, fazendo login na pagina web

--- testeselenium/src/testeselenium/com/br/TesteAuto.java
+++ testeselenium/src/testeselenium/com/br/TesteAuto.java
@@ -2,7 +2,9 @@ package testeselenium.com.br;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteAuto {
@@ -21,7 +23,15 @@ public class TesteAuto {
 	
     @Test
 	public void fluxo() {
-		driver.get("https://adx.doctum.edu.br/");
+		driver.get("http://localhost/SisBiblio/");
+		
+		WebElement usuario = driver.findElement(By.id("usuario"));
+		WebElement senha = driver.findElement(By.id("senha"));
+		WebElement botaologar = driver.findElement(By.name("botao"));
+		
+		usuario.sendKeys("genissonfg@gmail.com");
+		senha.sendKeys("123mudar");
+		botaologar.submit();
 		
     }
     
